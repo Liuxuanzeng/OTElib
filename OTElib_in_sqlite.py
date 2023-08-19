621712d2fe2e2e7da6635a407a834f7c85ce6013
Xuanzeng Liu <109024718+Liuxuanzeng@users.noreply.github.com>
2023-08-19 20:22:35 +0800

    Add files via upload

---
+++ OTElib_in_sqlite.py
@@ -0,0 +1,44 @@
+#coding:utf-8
+# by liuxuanzeng
+# 2023-5-7
+# oTElib中fasta导入sqlite3    为OTElib.db
+
+
+import pyfastx
+import sqlite3
+
+OTEfa = pyfastx.Fasta("12_spices_reantation.lib")
+db = sqlite3.connect("OTElib.db")
+cur =db.cursor()
+sql = '''CREATE TABLE if not exists OTElib (id text primary key, spname text, class text, familyn text, seq text)'''
+cur.execute(sql)
+z = 1
+for seq in OTEfa:
+    rawname = seq.name
+    species1 = rawname.split("__")[1]
+    speciesname = species1.split("-")[0]
+    TEname = rawname.split("#")[1]
+    keyname = rawname.split('#')[0]
+    keyname = str(z) + '_' + keyname
+    z = z + 1
+    TEnamelist = TEname.split("/")
+    sequence = seq.seq
+    if len(TEnamelist) == 2:
+        Classn = TEnamelist[0]
+        familyn = TEnamelist[1]
+        inlist = (keyname, speciesname, Classn, familyn, sequence)
+    else:
+        Classn = TEnamelist[0]
+        inlist = (keyname, speciesname, Classn, None, sequence)
+    cur.execute('INSERT INTO OTElib VALUES(?,?,?,?,?)', (inlist[0], inlist[1], inlist[2], inlist[3], inlist[4]))
+
+db.commit()
+cur.close()
+db.close()
+
+db = sqlite3.connect("OTElib.db")
+cur =db.cursor()
+
+
+
+
